549b6c318cca5e74229cc687c3b7b0c8119bc0d3
BlackZD <loganmullahh@gmail.com>
2026-04-24 11:26:32 +0200

    fix database and frontend code

--- backend/db.js
+++ backend/db.js
@@ -4,18 +4,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "huguka_e_learning",
-  
+   
+    host: process.env.DB_HOST || "a6grk6.h.filess.io",
+    port: process.env.DB_PORT || 61002,
+    user: process.env.DB_USER || "Huguka_e_learningDB_trickbirds",
+    password: process.env.DB_PASSWORD || "9e928aa3b30a8b2cc6958a89abfb2196300bfdaf",
+    database: process.env.DB_NAME || "Huguka_e_learningDB_trickbirds",
+    
+   
+    waitForConnections: true,
+    connectionLimit: 5, 
+    queueLimit: 0
 }); 
-
 
 try {
     const connection = await db.getConnection();
     console.log("✅ Database connection successful!");
-    connection.release(); // Important: release it back to the pool
+    connection.release(); 
 } catch (error) {
     console.error("⛔ Database connection failed:", error.message);
 }
